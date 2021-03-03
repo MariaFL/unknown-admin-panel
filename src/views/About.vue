7cca9455d5487690e7485b4e04f1c34f27676dbe
MariaFL <fedmaria2@gmail.com>
2021-03-03 18:32:52 +0500

    create new page for formulas

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>It my first pet project on vue 3.0. I want to do something like admin panel</p>
   </div>
 </template>
---
+++ src/views/CreateFormula.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Create your formula</h1>
+        <form action="">
+            <input type="text">
+            <button>Create picture</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CreateFormula',
+};
+</script>
+
+<style scoped>
+
+</style>
